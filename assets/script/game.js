443adbe3b705f31466976c1da827c677f9c764b4
andynicoo <811315800@qq.com>
2019-05-07 21:07:08 +0800

    动态创建

--- assets/script/game.js
+++ assets/script/game.js
@@ -9,11 +9,17 @@ cc.Class({
     onLoad () {
         cc.director.getPhysicsManager().enabled = true;
         cc.director.getPhysicsManager().debugDrawFlags = /* cc.PhysicsManager.DrawBits.e_aabbBit | */ cc.PhysicsManager.DrawBits.e_pairBit | cc.PhysicsManager.DrawBits.e_centerOfMassBit | cc.PhysicsManager.DrawBits.e_jointBit | cc.PhysicsManager.DrawBits.e_shapeBit;
-        this.down({a:'1'},'1.json')
     },
     start () {
-        cc.loader.loadRes("bbbbb.json", function (err, aa) {
-            // console.log(aa,111)
+        let that = this
+        cc.loader.loadRes('/level/level1.json', function (err, o) {
+            let nodes = o.json.nodes
+            if(nodes.length){
+                nodes.forEach((element)=>{
+                    that.createNode(element)
+                })
+            }
+
             // cc.loader.load(aa.nativeUrl, function(err, result2){
             //     console.log(result2);
             // });
@@ -24,6 +30,25 @@ cc.Class({
         // joint.anchor = new cc.Vec2(-10,0);
         // joint.connectedAnchor = new cc.Vec2(10,0);
         // joint.connectedBody = prefabB.getComponent(cc.RigidBody);
+    },
+    // 创建节点、加载sprite
+    createNode(element){
+        let that = this;
+        cc.loader.loadRes('/texture/' + element.self.name, cc.SpriteFrame,function(err,spriteFrame){
+            let node = new cc.Node(element.self.name)
+            node.position = element.self.position
+            node.width = element.self.width
+            node.height = element.self.height
+            let sprite = node.addComponent(cc.Sprite)
+            sprite.spriteFrame = spriteFrame
+            cc.find('Canvas').addChild(node);
+            that.createRigidBody(node,element.rigidBody)
+        })
+    },
+    createRigidBody(node,rigidInfo){
+        let rigidBody = node.addComponent(cc.RigidBody)
+        rigidBody.type = 'Static'
+        console.log(rigidBody)
     },
     down (content, filename) {
         var eleLink = document.createElement('a');
